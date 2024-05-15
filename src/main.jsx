7f2ba6205fdb4b413d17cb2894ccec5e3d92106c
simpleton <chazesingh@gmail.com>
2024-05-15 14:59:57 +0530

    ADD: router outlet ,CONF: tailwind css into vite

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children: [
+    //   { path: "/", element: <Home /> }, 
+    // ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <ThemeProvider>
+  <AuthProvider>
+    <RouterProvider router={router}/>
+  </AuthProvider>
+  </ThemeProvider>
 )
