ef70ef03eea94c9734dc4af55cfe5659bdeaf935
simpleton <chazesingh@gmail.com>
2024-05-20 13:33:38 +0530

    POS: changing the layout for cross and origin.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,31 +9,34 @@ const Home = () => {
     <div className="h-full w-full">
       {/* sub-parent containing the black box */}
       <div className="w-full h-full bg-black flex items-center flex-col">
+        {/* using another parent div to fix the position acc to the rectange div */}
+        <div className="w-4/6 h-1/2">
         {/* child element containing card data starts here */}
-        <div className="w-4/6 h-1/2 mx-auto border-2 border-white rounded-3xl  bg-transparent relative top-32">
-         <div className="absolute -top-24 -left-32 ">
+        <div className="w-full h-full mx-auto border-2 border-white rounded-3xl  bg-white/10 relative top-32 backdrop-blur-lg backdrop-filter shadow-lg z-10 ">
+        <div className="absolute  -top-0 -right-16  hidden sm:block ">
+            <img src={skateBoard} alt="Home Banner skate boards image" className="object-cover object-center h-96 scale-150" />
+          </div>
+        </div>
+        
+         {/* <div className="isolate aspect-video w-96 rounded-xl bg-white/50 shadow-lg ring-1 ring-black/5"> */}
+         <div className="absolute top-6 left-56   ">
             <img
-              className="object-cover object-center h-64"
+              className="object-cover object-center h-64 first-letter:-z-10"
               src={neonCross}
               alt="Home Banner cross neon image"
             />
           </div>
           {/* div containing the neon cross for the home banner ends here*/}
           {/* div containg the neon origin for the home banner starts here */}
-          <div className="absolute -bottom-36 left-1/2 transform -translate-x-1/2">
+          <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2  ">
             <img
-              className="object-cover object-center h-64 "
+              className="object-cover object-center h-72 "
               src={neonOrigin}
               alt="Home Banner origin neon image"
             />
-          </div>
-          <div className="absolute  -top-4 -right-16">
-            <img src={skateBoard} alt="Home Banner skate boards image" className="object-cover object-center h-96 scale-150" />
-          </div>
-        </div>
-         {/* div containing the neon cross for the home banner starts here */}
+          </div></div>
           
-          {/* div containing the neon origin for the home banner ends here */}
+         
 
         {/* child element containing card data ends here */}
       </div>
