1dcbd24252a8158790778ba369393126e9b03ee9
Aashish <aashish11202@gmail.com>
2024-05-22 15:29:30 +0530

    REM: App.css resulting fixed of content shift from body, ADD :Componet for skateimpact in home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import neonCross from "../../assets/neonCross.png";
 import neonOrigin from "../../assets/neonOrigin.png";
 import skateBoard from "../../assets/skateImage.png";
+import SkateImpact from "../SkateImpact";
 
 const Home = () => {
   return (
     //parent div starts here
     <div className="h-full w-full">
       {/* sub-parent containing the black box */}
-      <div className="w-full h-full bg-black flex items-center flex-col">
+      <div className="w-full h-full bg-black flex items-center flex-col nonsense ">
         {/* using another parent div to fix the position acc to the rectange div */}
         <div className="w-4/6 h-1/2">
         {/* child element containing card data starts here */}
@@ -41,6 +42,7 @@ const Home = () => {
         {/* child element containing card data ends here */}
       </div>
       {/* parent div ends here */}
+      <SkateImpact/>
     </div>
   );
 };
