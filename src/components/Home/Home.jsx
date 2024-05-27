b72e988afb02da9ed7129d4c53f923075c15ee6b
Aashish <aashish11202@gmail.com>
2024-05-27 15:02:56 +0530

    TEST:SkateImapct comp on home page,ADD:Bounce To SkateBoard Image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import neonCross from "../../assets/neonCross.png";
 import neonOrigin from "../../assets/neonOrigin.png";
-import skateBoard from "../../assets/skateImage.png";
+import kateBoard from "../../assets/skateImage.png";
 import HomeBanner from "./HomeBanner";
 import NeonCard from "../NeonCard";
 import HomeBlogCard from "../Cards/HomeBlogCard";
+import SkateImpact from "../SkateImpact";
 
 const Home = () => {
   return (
     
     <>
       {/* <HomeBanner/> */}
-      <HomeBlogCard/>
+      {/* <HomeBlogCard/> */}
+     {/* <SkateImpact/> */}
     </>
   );
 };
