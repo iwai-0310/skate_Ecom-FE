b0be6f827c4ccd2bf9adf91bd3cdbec32bf95dfd
Aashish <aashish11202@gmail.com>
2024-06-03 23:02:43 +0530

    Creating new branch

--- src/components/Cards/SwiperEffect.jsx
+++ src/components/Cards/SwiperEffect.jsx
@@ -1,27 +1,24 @@
-import React from 'react'
-import {Swiper,SwiperSlide} from 'swiper'
+import React from "react"
+import {Swiper, SwiperSlide} from "swiper"
 
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css"
+import "swiper/css/effect-coverflow"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
 
-import { EffectCoverflow,Pagination,Navigation } from 'swiper/modules';
+import {EffectCoverflow, Pagination, Navigation} from "swiper/modules"
 
 import slide_image from "../../assets/neonCross.png"
-
 
 const SwiperEffect = () => {
   return (
     <div className="container">
-        <div className="heading">
-            Slider Effect
-        </div>
-        <Swiper>
-            <SwiperSlide>
-                <img src={slide_image} alt="neon" />
-            </SwiperSlide>
-        </Swiper>
+      <div className="heading">Slider Effect</div>
+      <Swiper>
+        <SwiperSlide>
+          <img src={slide_image} alt="neon" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
